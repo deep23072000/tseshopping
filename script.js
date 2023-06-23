8cd750fa3b94fa92d483297b146a4d4c75db7109
deep23072000 <114299553+deep23072000@users.noreply.github.com>
2023-06-23 21:51:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,523 @@
+
+
+
+
+var home = () => {
+  const a = `<div class="homemega">
+
+    <ul>
+      <li><a href="index.html">home</a></li>
+      <li><a href="#">Vougue</a></li>
+      <li><a href="#">Elegant</a></li>
+      <li><a href="#">Trendy style</a></li>
+      <li><a href="#">Vougue</a></li>
+      <li><a href="#">Feminin</a></li>
+      <li><a href="#">Instyle</a></li>
+      <li><a href="#">Chic</a></li>
+      <li><a href="#">Glamour</a></li>
+      <li><a href="#">Hallobean</a></li>
+    </ul>
+    <ul>
+      <li><a href="#">home</a></li>
+      <li><a href="#">Vougue</a></li>
+      <li><a href="#">Elegant</a></li>
+      <li><a href="#">Trendy style</a></li>
+      <li><a href="#">Vougue</a></li>
+      <li><a href="#">Feminin</a></li>
+      <li><a href="#">Instyle</a></li>
+      <li><a href="#">Chic</a></li>
+      <li><a href="#">Glamour</a></li>
+      <li><a href="#">Hallobean</a></li>
+    </ul>
+    <ul>
+      <li><a href="#"> home</a></li>
+      <li><a href="#">Vougue</a></li>
+      <li><a href="#">Elegant</a></li>
+      <li><a href="#">Trendy style</a></li>
+      <li><a href="#">Vougue</a></li>
+      <li><a href="#">Feminin</a></li>
+      <li><a href="#">Instyle</a></li>
+      <li><a href="#">Chic</a></li>
+      <li><a href="#">Glamour</a></li>
+      <li><a href="#">Hallobean</a></li>
+    </ul>
+    <ul>
+      <li><a href="#">Main</a></li>
+      <li><a href="#">Vougue</a></li>
+      <li><a href="#">Elegant</a></li>
+      <li><a href="#">Trendy style</a></li>
+      <li><a href="#">Vougue</a></li>
+      <li><a href="#">Feminin</a></li>
+      <li><a href="#">Instyle</a></li>
+      <li><a href="#">Chic</a></li>
+      <li><a href="#">Glamour</a></li>
+      <li><a href="#">Hallobean</a></li>
+    </ul>
+    <ul>
+      <li><a href="#"> home</a></li>
+      <li><a href="#">Vougue</a></li>
+      <li><a href="#">Elegant</a></li>
+      <li><a href="#">Trendy style</a></li>
+      <li><a href="#">Vougue</a></li>
+      <li><a href="#">Feminin</a></li>
+      <li><a href="#">Instyle</a></li>
+      <li><a href="#">Chic</a></li>
+      <li><a href="#">Glamour</a></li>
+      <li><a href="#">Hallobean</a></li>
+    </ul>
+
+    <ul>
+      <li><a href="#"> home</a></li>
+      <li><a href="#">Vougue</a></li>
+      <li><a href="#">Elegant</a></li>
+      <li><a href="#">Trendy style</a></li>
+      <li><a href="#">Vougue</a></li>
+      <li><a href="#">Feminin</a></li>
+      <li><a href="#">Instyle</a></li>
+      <li><a href="#">Chic</a></li>
+      <li><a href="#">Glamour</a></li>
+      <li><a href="#">Hallobean</a></li>
+    </ul>
+  </div>`;
+  const b = "";
+  document.getElementById("demos").innerHTML = a;
+  document.getElementById("shops").innerHTML = b;
+  document.getElementById("pages").innerHTML = b;
+  document.getElementById("products").innerHTML = b;
+  document.getElementById("blogs").innerHTML = b;
+  document.getElementById("foxkits").innerHTML = b;
+};
+var home1 = () => {
+  const a = "";
+  document.getElementById("demos").innerHTML = a;
+};
+
+// =================  home section end =============================
+
+var shop = () => {
+  const a = `<div class="homemega">
+
+  <ul>
+    <li><a href="buyNow.html">shop</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Elegant</a></li>
+    <li><a href="#">Trendy style</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Feminin</a></li>
+    <li><a href="#">Instyle</a></li>
+    <li><a href="#">Chic</a></li>
+    <li><a href="#">Glamour</a></li>
+    <li><a href="#">Hallobean</a></li>
+  </ul>
+  <ul>
+    <li><a href="#">home</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Elegant</a></li>
+    <li><a href="#">Trendy style</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Feminin</a></li>
+    <li><a href="#">Instyle</a></li>
+    <li><a href="#">Chic</a></li>
+    <li><a href="#">Glamour</a></li>
+    <li><a href="#">Hallobean</a></li>
+  </ul>
+  <ul>
+    <li><a href="#"> home</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Elegant</a></li>
+    <li><a href="#">Trendy style</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Feminin</a></li>
+    <li><a href="#">Instyle</a></li>
+    <li><a href="#">Chic</a></li>
+    <li><a href="#">Glamour</a></li>
+    <li><a href="#">Hallobean</a></li>
+  </ul>
+  <ul>
+    <li><a href="#">Main</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Elegant</a></li>
+    <li><a href="#">Trendy style</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Feminin</a></li>
+    <li><a href="#">Instyle</a></li>
+    <li><a href="#">Chic</a></li>
+    <li><a href="#">Glamour</a></li>
+    <li><a href="#">Hallobean</a></li>
+  </ul>
+  <ul>
+    <li><a href="#"> home</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Elegant</a></li>
+    <li><a href="#">Trendy style</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Feminin</a></li>
+    <li><a href="#">Instyle</a></li>
+    <li><a href="#">Chic</a></li>
+    <li><a href="#">Glamour</a></li>
+    <li><a href="#">Hallobean</a></li>
+  </ul>
+
+  <ul>
+    <li><a href="#"> home</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Elegant</a></li>
+    <li><a href="#">Trendy style</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Feminin</a></li>
+    <li><a href="#">Instyle</a></li>
+    <li><a href="#">Chic</a></li>
+    <li><a href="#">Glamour</a></li>
+    <li><a href="#">Hallobean</a></li>
+  </ul>
+</div>`;
+  const b = "";
+  document.getElementById("shops").innerHTML = a;
+  document.getElementById("demos").innerHTML = b;
+  document.getElementById("pages").innerHTML = b;
+  document.getElementById("foxkits").innerHTML = b;
+  document.getElementById("blogs").innerHTML = b;
+  document.getElementById("products").innerHTML = b;
+};
+var shop1 = () => {
+  const a = "";
+  document.getElementById("shops").innerHTML = a;
+};
+
+
+
+// =========================this is products section==================
+
+var product = () => {
+  const a = `<div class="homemega">
+
+  <ul>
+    <li><a href="#">home</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Elegant</a></li>
+    <li><a href="#">Trendy style</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Feminin</a></li>
+    <li><a href="#">Instyle</a></li>
+    <li><a href="#">Chic</a></li>
+    <li><a href="#">Glamour</a></li>
+    <li><a href="#">Hallobean</a></li>
+  </ul>
+  <ul>
+    <li><a href="#">home</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Elegant</a></li>
+    <li><a href="#">Trendy style</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Feminin</a></li>
+    <li><a href="#">Instyle</a></li>
+    <li><a href="#">Chic</a></li>
+    <li><a href="#">Glamour</a></li>
+    <li><a href="#">Hallobean</a></li>
+  </ul>
+  <ul>
+    <li><a href="#"> home</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Elegant</a></li>
+    <li><a href="#">Trendy style</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Feminin</a></li>
+    <li><a href="#">Instyle</a></li>
+    <li><a href="#">Chic</a></li>
+    <li><a href="#">Glamour</a></li>
+    <li><a href="#">Hallobean</a></li>
+  </ul>
+  <ul>
+    <li><a href="#">Main</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Elegant</a></li>
+    <li><a href="#">Trendy style</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Feminin</a></li>
+    <li><a href="#">Instyle</a></li>
+    <li><a href="#">Chic</a></li>
+    <li><a href="#">Glamour</a></li>
+    <li><a href="#">Hallobean</a></li>
+  </ul>
+  <ul>
+    <li><a href="#"> home</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Elegant</a></li>
+    <li><a href="#">Trendy style</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Feminin</a></li>
+    <li><a href="#">Instyle</a></li>
+    <li><a href="#">Chic</a></li>
+    <li><a href="#">Glamour</a></li>
+    <li><a href="#">Hallobean</a></li>
+  </ul>
+
+  <ul>
+    <li><a href="#"> home</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Elegant</a></li>
+    <li><a href="#">Trendy style</a></li>
+    <li><a href="#">Vougue</a></li>
+    <li><a href="#">Feminin</a></li>
+    <li><a href="#">Instyle</a></li>
+    <li><a href="#">Chic</a></li>
+    <li><a href="#">Glamour</a></li>
+    <li><a href="#">Hallobean</a></li>
+  </ul>
+</div>`;
+  const b = "";
+  document.getElementById("products").innerHTML = a;
+  document.getElementById("shops").innerHTML = b;
+  document.getElementById("demos").innerHTML = b;
+  document.getElementById("pages").innerHTML = b;
+  document.getElementById("blogs").innerHTML = b;
+  document.getElementById("foxkits").innerHTML = b;
+
+};
+var product1 = () => {
+  const a = "";
+  document.getElementById("products").innerHTML = a;
+};
+
+// ========================end products section=======================
+
+
+
+// ==========================this is page section======================
+var page = () => {
+
+  const save =
+    `
+    `
+
+  let b = "";
+  document.getElementById("pages").innerHTML = save;
+  document.getElementById("demos").innerHTML = b;
+  document.getElementById("products").innerHTML = b;
+  document.getElementById("foxkits").innerHTML = b;
+  document.getElementById("blogs").innerHTML = b;
+  document.getElementById("shops").innerHTML = b;
+};
+
+var page1 = () => {
+  let a = "";
+  document.getElementById("pages").innerHTML = a;
+}
+
+//=========================end page section========================
+
+
+// ==========================this blog section======================
+var blog = () => {
+  const items = ["Grid layout",
+    "List view",
+    "Blog with left sidebar",
+    "Blog with right sidebar",
+    "Single post style 1",
+    "Single post style 2",
+    "Single post with sidebar"
+  ];
+  const save = items
+    .map(
+      (item) => `
+      <div class="homemega2">
+    <li><a href="#">${item}</a></li>
+    </div>
+    `
+    )
+    .join("");
+  let d = ""
+  document.getElementById("blogs").innerHTML = save;
+  document.getElementById("demos").innerHTML = d;
+  document.getElementById("foxkits").innerHTML = d;
+  document.getElementById("products").innerHTML = d;
+  document.getElementById("shops").innerHTML = d;
+  document.getElementById("pages").innerHTML = d;
+};
+
+var blog1 = () => {
+  let a = "";
+  document.getElementById("blogs").innerHTML = a;
+}
+
+//=========================end blog section========================
+
+// =======================start foxkits=================================
+var foxkit = () => {
+  const items = ["Grid layout",
+    "List view",
+    "Blog with left sidebar",
+    "Blog with right sidebar",
+    "Single post style 1",
+    "Single post style 2",
+    "Single post with sidebar"
+  ];
+  const save = items
+    .map(
+      (item) => `
+      <div class="homemega2">
+    <li><a href="#">${item}</a></li>
+    </div>
+    `
+    )
+    .join("");
+  let d = "";
+  document.getElementById("foxkits").innerHTML = save;
+  document.getElementById("demos").innerHTML = d;
+  document.getElementById("blogs").innerHTML = d;
+  document.getElementById("products").innerHTML = d;
+  document.getElementById("shops").innerHTML = d;
+  document.getElementById("pages").innerHTML = d;
+};
+
+var foxkit1 = () => {
+  let a = "";
+  document.getElementById("foxkits").innerHTML = a;
+}
+
+
+// ========================end foxkits========================
+
+
+
+// ===========season collection======================
+
+const a = () => {
+  const items = [{
+    id: 1,
+    "name": "Leather Bags",
+    "item": "4 items",
+    "img": './images/leatherbag.webp',
+  },
+  {
+    id: 2,
+    "name": "Glasses",
+    "item": "1 items",
+    "img": './images/shoes.webp',
+  },
+  {
+    id: 3,
+    "name": "Tops",
+    "item": "6 items",
+    "img": './images/tops.webp',
+  },
+  {
+    id: 4,
+    "name": "Blazers",
+    "item": "2 items",
+    "img": './images/blazer.webp',
+  }
+  ];
+
+  const save = items.map((item) => {
+    return `
+    <div class="card bg-dark text-black" id="card1">
+    <img src="${item.img}" class="card-img" alt="...">
+    <div class="card-img-overlay">
+      <h5 class="card-title">${item.name}</h5>
+      <p class="card-text">${item.item}</p>
+    </div>
+  </div>
+  `})
+    .join("");
+  console.log("h3");
+
+  document.getElementById('season').innerHTML = save;
+}
+// ============================================================================
+const bestseller = () => {
+
+  const items = [{
+    id: 1,
+    "name": "Wool-blend jacket",
+    "prize": "$ 33",
+    "img": './images/portion2/im1.webp',
+  },
+  {
+    id: 2,
+    "name": "Stripe cotton shirt",
+    "prize": "$ 13",
+    "img": './images/portion2/im2.webp',
+  },
+  {
+    id: 3,
+    "name": "Slim High Ankle jeans",
+    "prize": "$ 20",
+    "img": './images/portion2/im3.webp',
+  },
+  {
+    id: 4,
+    "name": "Oversized T-shirt",
+    "prize": "$ 23",
+    "img": './images/portion2/im4.webp',
+  },
+  {
+    id: 5,
+    "name": "Oversized Jacket",
+    "prize": "$ 40",
+    "img": './images/portion2/im5.webp',
+  },
+  {
+    id: 6,
+    "name": "Long-Sleeve Maxy Dress",
+    "prize": "$ 19",
+    "img": './images/portion2/im6.webp',
+  },
+  {
+    id: 7,
+    "name": "Linen Check Blazers",
+    "prize": "$ 45",
+    "img": './images/portion2/im7.webp',
+  },
+  {
+    id: 8,
+    "name": "Linen blend shirt",
+    "prize": "$ 34",
+    "img": './images/portion2/im8.webp',
+  },
+  {
+    id: 9,
+    "name": "Denim Jacket",
+    "prize": "$ 67",
+    "img": './images/portion2/im9.webp',
+  },
+  {
+    id: 10,
+    "name": "Cotton Falenal Jacket",
+    "prize": "$ 78",
+    "img": './images/portion2/im10.webp',
+  },
+  ];
+
+
+
+  const save = items
+    .map((item) => {
+      const store1 = `
+      <center>
+  
+  <div  id="bestie">  
+   <img src="${item.img}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <p style="font-weight:500;font-size:12px">${item.name}</p>
+    <p class="card-text">${item.prize}</p>
+    <div style="display:flex;">
+    <p class="card-text" style="height:17px;width:17px;background-color:red;border-radius:50%;opacity:40%;"></p>
+    <p class="card-text" style="height:17px;width:17px;background-color:purple;border-radius:50%;opacity:40%;margin-left:5px"></p>
+    </div>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item" style="text-align:center;background-color:black;color:white;border-radius:6px;" > Shop Now </li>
+  </ul>
+</div></center>
+
+  `;
+      return store1;
+    })
+    .join("");
+
+  document.getElementById("bestchild").innerHTML = save;
+
+
+}
+
